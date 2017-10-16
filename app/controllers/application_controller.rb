c81a45d2a498c6eb8fc712bfeaaea75d67487798
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 15:41:41 -0700

    add #require_login and #save_and_flash to app_ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,28 @@ private
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+protected
+  def require_login
+    @user = User.find_by(id: session[:user_id])
+    unless @user
+      flash[:status] = :failure
+      flash[:message] = "You must be logged in to do that!"
+      redirect_to root_path
+    end
+  end
+
+  def save_and_flash(model)
+    result = model.save
+
+    if result
+      flash[:status] = :success
+      flash[:message] = "Successfully saved #{model.class} #{model.id}"
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to save #{model.class}"
+      flash.now[:details] = model.errors.messages
+    end
+
+    return result
+  end
 end
