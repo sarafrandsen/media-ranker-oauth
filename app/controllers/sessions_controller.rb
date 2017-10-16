dd0f07d92ffeae7886ab0cdc32ac5a666ef90e07
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 15:40:21 -0700

    add from_auth_has to user.rb, #login #logout to sess_ctrl.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,34 +6,37 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     raise
   end
-  
+
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      user = User.find_by(provider: params[:provider], uid: auth_hash['uid'])
+      if user.nil?
+        # User has not logged in before
+        # Create a new record in the DB
+        user = User.from_auth_hash(params[:provider], auth_hash)
+        save_and_flash(user)
+
+      else
         session[:user_id] = user.id
         flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:message] = "Successfully logged in as returning user #{user.name}"
+
       end
+
+    else
+      flash[:status] = :failure
+      flash[:message] = "Could not create user from OAuth data"
     end
+
     redirect_to root_path
   end
 
   def logout
     session[:user_id] = nil
     flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    flash[:message] = "You have been logged out"
     redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.from_auth_hash(provider, auth_hash)
+    user = new
+    user.provider = provider
+    user.uid = auth_hash['uid']
+    user.name = auth_hash['info']['name']
+    user.email = auth_hash['info']['email']
+    user.username = auth_hash['info']['nickname']
+
+    return user
+  end
 end
