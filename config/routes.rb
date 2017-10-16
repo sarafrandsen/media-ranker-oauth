64bc2834c3a9af53ced1eb98e413cfc9b5571d53
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-16 15:55:46 -0700

    add link_to github auth in app view, tinkering with routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
   get '/login', to: 'sessions#login_form', as: 'login'
-  post '/login', to: 'sessions#login'
+  post '/auth/:provider/callback', to: 'sessions#login'
   post '/logout', to: 'sessions#logout', as: 'logout'
 
   resources :works
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
   
-  get "/auth/:provider/callback", to: "sessions#create"
 end
